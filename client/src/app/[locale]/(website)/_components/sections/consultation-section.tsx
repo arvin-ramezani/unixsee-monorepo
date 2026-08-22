40722e43e203c4248253b4848d742f397a30c76e
Arvin <rvin.ramezani@gmail.com>
2026-08-21 21:19:07 -0700

    Combine faq-section and consultant-section together in home page.

--- client/src/app/[locale]/(website)/_components/sections/consultation-section.tsx
+++ client/src/app/[locale]/(website)/_components/sections/consultation-section.tsx
@@ -4,6 +4,21 @@ import Section from "@/components/common/section";
 import ConsultationTabsSection from "../others/consultation-tabs-section";
 import { cn } from "@/lib/utils";
 import { ScaleTitle } from "@/components/common/motion/scale-title";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const faqItemKeys = [
+  "woocommerceOnly",
+  "managedServer",
+  "migration",
+  "fixedPlans",
+  "complementaryServices",
+  "bestFit",
+] as const;
 
 export type ConsultationSectionType = { id?: string };
 
@@ -38,6 +53,49 @@ export default function ConsultationSection({ id }: ConsultationSectionType) {
           </ScaleTitle>
 
           <ConsultationTabsSection className="2xl:mt-22" />
+        </Section>
+
+        <Section className="min-h-auto">
+          <ScaleTitle
+            as={"h2"}
+            scaleFrom={0.6}
+            scaleTo={1}
+            transformOrigin="center center"
+            className="flex justify-center"
+          >
+            {t(`FAQSection.title`)}
+          </ScaleTitle>
+          <ScaleTitle
+            as={"p"}
+            scaleFrom={0.6}
+            scaleTo={1}
+            transformOrigin="center center"
+            className="mt-4 flex justify-center text-center lg:mt-6"
+          >
+            {t(`FAQSection.description`)}
+          </ScaleTitle>
+
+          <div className="w-full items-center justify-between gap-16 2xl:flex">
+            <Accordion
+              type="multiple"
+              className="mx-auto mt-8 max-w-2xl lg:mt-12 2xl:mx-0"
+            >
+              {faqItemKeys.map((item) => (
+                <AccordionItem
+                  key={t(`FAQSection.items.${item}.question` as never)}
+                  value={t(`FAQSection.items.${item}.question` as never)}
+                >
+                  <AccordionTrigger className="py-4 no-underline! lg:py-8">
+                    {t(`FAQSection.items.${item}.question` as never)}
+                  </AccordionTrigger>
+
+                  <AccordionContent>
+                    {t(`FAQSection.items.${item}.answer` as never)}
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
         </Section>
       </div>
     </div>
